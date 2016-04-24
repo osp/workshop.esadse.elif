82c6ed86f8395f5a271e7e92cfaa149b31496afe
Ludi <hello@ludi.be>
2016-04-25 00:00:16 +0200

    live pictures

--- setup/setup.js
+++ setup/setup.js
@@ -1,4 +1,3 @@
 // Local settings
 var nb_page = 8;
-var content = "http://pads.osp.kitchen/p/st-etienne-2016/export/txt";
-
+var content = "http://pads.osp.kitchen/p/amours-imaginaires/export/txt";
